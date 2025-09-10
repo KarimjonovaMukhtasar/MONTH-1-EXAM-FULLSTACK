94186a73e15a4a60937d84f7209455c52558d58e
KarimjonovaMukhtasar <karimjonovamukhtasar2003@gmail.com>
2025-09-10 21:53:48 +0500

    TASK FULLY DONE

--- 12.js
+++ 12.js
@@ -17,13 +17,86 @@ function hoursBetweenDays(day1, day2) {
 }
 ```
 */
-function hoursBetweenDays(day1, day2) {
-    for(day1.includes("OCT")){
-
+function setMonth(str){
+    let day1 = str.toLowerCase()
+    if(day1.includes("oct")){
+        return "9"
     }
-    let time1 = new Date(day1)
-    let time2 = new Date(day2)
-    let res = Math.abs(time2- time1)/(1000 * 60 * 60)
+    else if(day1.includes("nov")){
+        return "10"
+    }
+    else if(day1.includes("dec")){
+        return "11"
+    }
+    else if(day1.includes("jan")){
+        return "0"
+    }
+    else if(day1.includes("feb")){
+        return "1"
+    }
+     else if(day1.includes("march")){
+        return "2"
+    }
+    else if(day1.includes("april")){
+        return "3"
+    }
+     else if(day1.includes("may")){
+        return "4"
+    }
+    else if(day1.includes("june")){
+        return "5"
+    }
+     else if(day1.includes("july")){
+        return "6"
+    }
+    else if(day1.includes("august")){
+        return "7"
+    }
+    else if(day1.includes("sep")){
+        return "8"
+    }
+    else{
+        return "NOT FOUND SUCH A MONTH, ENTER THE CORRECT MONTH NAME"
+    }
+}
+function hoursBetweenDays(day1, day2) {
+    let day = ""
+    for(let i = 0; i < day1.length; i++){
+        if(!isNaN(day1[i])){
+           day += day1[i]
+        }
+    }
+    let time1 = new Date()
+    time1.setDate(Number(day))
+    let month1 = setMonth(day1)
+    time1.setMonth(+month1)
+    console.log(time1)
+    let day0 = ""
+    for(let i = 0; i < day2.length; i++){
+        if(!isNaN(day2[i])){
+           day0 += day2[i]
+        }
+    }
+    let time2 = new Date()
+    time2.setDate(Number(day0))
+    let month2 = setMonth(day2)
+    time2.setMonth(+month2)
+    console.log(time2)
+    
+
+    // DAYS AND MONTHS
+    // let x = ""
+    // x += time1.getDate()
+    // let d = time1.getMonth()
+
+    // let y = ""
+    // y += time2.getDate()
+    // y += "  " + time2.getMonth()
+    let str = `${time2.getFullYear()}-${time2.getMonth()}-${time2.getDate()}`
+    let str1 = `${time1.getFullYear()}-${time1.getMonth()}-${time1.getDate()}`
+    let x = new Date(str)
+    let y = new Date(str1)
+    let res = Math.abs(x-y)/(1000 * 60 * 60)
     return res
 }
-console.log(hoursBetweenDays("25-october", "oktyabrning 27chisi"))
+console.log(hoursBetweenDays("25 october", "octoberning 27chisi"))
--- 7.js
+++ 7.js
@@ -1,27 +1,37 @@
 /*
 ## Question 7: Remove K Digits to Get Smallest Number
 Remove k digits from a number string to create the smallest possible number.
-
 **Example:**
 ```javascript
 Input: num = "1432219", k = 3
 Output: "1219"
 ```
-
 **Function Signature:**
 ```javascript
 function removeKDigits(num, k) {
     // Your code here
 }
 ```
-
 */
 
-// MATEMATIK BILIMIM YETARLI EMAS, ISHLOLMADIM , UZR :( in shaa Allah matematikani algoritmni kelajakda o'rganamiz...)
+
+// MATEMATIK BILIMIM YETARLI EMAS, Saytdan foydalandim :(. UZR :( in shaa Allah matematikani algoritmni kelajakda o'rganamiz...)
 function removeKDigits(num, k) {
-    let res = num
-    for(let i=0; i < k; i++){
-        if()
+    if(k === 0){
+        return num
+    }
+    else{
+        let size = num.length
+        if(size <= k){
+            return ""
+        }
+        let minIndex = 0
+        for(let i=0; i <= k; i++)
+        if(num[i] < num[minIndex]){
+            minIndex = i
+        }
+        return num[minIndex] + removeKDigits(num.slice(minIndex + 1), k-minIndex)
     }
 }
+console.log(removeKDigits("1432219", 3))
 
